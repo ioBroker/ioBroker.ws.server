6093a925fbdf825f44188b3f787a8e2e4bd052ec
GermanBluefox <dogafox@gmail.com>
2022-01-30 21:06:53 +0100

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,54 @@
+/* jshint -W097 */
+/* jshint strict: false */
+/* jslint node: true */
+/* jshint expr: true */
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('Implement tests', () => {
+    const socket = require('../index');
+    const http = require('http');
+
+    it('Test', done => {
+        const requestListener = function (req, res) {
+            res.writeHead(200);
+            res.end('Hello, World!');
+        };
+
+        // create web server
+        const webServer    = http.createServer(requestListener);
+        // create web socket server
+        const socketServer = socket.listen(webServer, {pingInterval: 120000, pingTimeout: 30000});
+
+        // install event handlers on socket connection
+        function onConnection(socket, initDone) {
+            console.log('==> Connected IP: ' + socket.connection.remoteAddress);
+
+            socket.on('message', function (data, cb) {
+                console.log('Received ' + data);
+                cb(data + 1);
+            });
+
+            socket.on('disconnect', function (error) {
+                console.log(`<== Disconnect from ${socket.connection.remoteAddress}: ${error}`);
+            });
+
+            initDone && initDone();
+        }
+
+        // install event handlers of the socket server
+        socketServer.on('connection', onConnection);
+        socketServer.on('error', (e, details) => {
+            console.error(`Server error: ${e}${details ? ' - ' + details : ''}`);
+        });
+
+        // start web server
+        webServer.listen(5000);
+
+        setTimeout(() => {
+            webServer.close();
+            done();
+        }, 3000);
+    }).timeout(4000);
+});
